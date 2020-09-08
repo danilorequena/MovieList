b15fa4af1c86d3f176f178b88983995f6f3dcaa5
Danilo Requena <danilo@requena.dev>
2020-09-07 23:54:05 -0300

    Implement a mainViewModel and more some refactorings

---
+++ ToDoMovie&Series/Models/SeriesOnAir.swift
@@ -0,0 +1,67 @@
+//
+//  SeriesOnAir.swift
+//  ToDoMovie&Series
+//
+//  Created by Danilo Requena on 07/09/20.
+//  Copyright © 2020 Danilo Requena. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - SeriesOnAir
+struct SeriesOnAir: Codable {
+    let page, totalResults, totalPages: Int?
+    let results: [ResultSeriesOnAir]?
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case results
+    }
+}
+
+// MARK: - Result
+struct ResultSeriesOnAir: Codable {
+    let originalName: String?
+    let genreIDS: [Int]?
+    let name: String?
+    let popularity: Double?
+    let originCountry: [OriginCountry]?
+    let voteCount: Int?
+    let firstAirDate: String?
+    let backdropPath: String?
+    let originalLanguage: OriginalLanguage?
+    let id: Int?
+    let voteAverage: Double?
+    let overview, posterPath: String?
+
+    enum CodingKeys: String, CodingKey {
+        case originalName = "original_name"
+        case genreIDS = "genre_ids"
+        case name, popularity
+        case originCountry = "origin_country"
+        case voteCount = "vote_count"
+        case firstAirDate = "first_air_date"
+        case backdropPath = "backdrop_path"
+        case originalLanguage = "original_language"
+        case id
+        case voteAverage = "vote_average"
+        case overview
+        case posterPath = "poster_path"
+    }
+}
+
+enum OriginCountry: String, Codable {
+    case ca = "CA"
+    case de = "DE"
+    case jp = "JP"
+    case us = "US"
+}
+
+enum OriginalLanguage: String, Codable {
+    case de = "de"
+    case en = "en"
+    case fr = "fr"
+    case ja = "ja"
+}
--- ToDoMovie&Series/Service/RequestAPI.swift
+++ ToDoMovie&Series/Service/RequestAPI.swift
@@ -19,12 +19,6 @@ enum Err {
 
 
 class RequestAPI {
-    private static let apiKey = "ddf20e1d6a0147313cfd3b4ac419e373"
-    private static let basePathMovies = "https://api.themoviedb.org/3/movie/popular?api_key="
-    private static let basePathPopularSeries = "https://api.themoviedb.org/3/tv/popular?api_key="
-    private static let basePathLatestSeries = "https://api.themoviedb.org/3/tv/top_rated?api_key="
-    
-    
     private static let configuration: URLSessionConfiguration = {
         let config = URLSessionConfiguration.default
         config.httpMaximumConnectionsPerHost = 40
@@ -34,7 +28,7 @@ class RequestAPI {
     
     
     class func loadMovies(onComplete: @escaping (Movie?) -> Void, onError: @escaping (Err) -> Void) {
-        guard let url = URL(string: basePathMovies + apiKey + "&language=pt-BR&page=1") else {
+        guard let url = URL(string: Constants.basePathMovies + Constants.apiKey + "&language=pt-BR&page=1") else {
             onError(.url)
             return
         }
@@ -69,7 +63,7 @@ class RequestAPI {
     
     
     class func loadPopularSeries(onComplete: @escaping (PopularSeries?) -> Void, onError: @escaping (Err) -> Void) {
-        guard let url = URL(string: basePathPopularSeries + apiKey + "&language=pt-BR&page=1") else {
+        guard let url = URL(string: Constants.basePathPopularSeries + Constants.apiKey + "&language=pt-BR&page=1") else {
             onError(.url)
             return
         }
@@ -103,7 +97,7 @@ class RequestAPI {
     }
     
     class func loadTopRatedSeries(onComplete: @escaping (TopRatedSeries?) -> Void, onError: @escaping (Err) -> Void) {
-        guard let url = URL(string: basePathLatestSeries + apiKey + "&language=en-US&page=1") else {
+        guard let url = URL(string: Constants.basePathLatestSeries + Constants.apiKey + "&language=en-US&page=1") else {
             onError(.url)
             return
         }
@@ -119,6 +113,40 @@ class RequestAPI {
                     do {
                         let serie = try JSONDecoder().decode(TopRatedSeries.self, from: data)
                         onComplete(serie)
+                        print("FetchOK")
+                    } catch let jsonErr {
+                        onError(.invalidJSON)
+                        print("Error serializing json:", jsonErr)
+                    }
+                } else {
+                   onError(.responseStatusCode(code: response.statusCode))
+                    print("Algo deu Errado no servidor")
+                }
+            } else {
+                onError(.taskError(error: error!))
+                print("Algo errado")
+            }
+        }
+        dataTask.resume()
+    }
+    
+    class func loadSeriesOnAir(onComplete: @escaping (SeriesOnAir?) -> Void, onError: @escaping (Err) -> Void) {
+        guard let url = URL(string: Constants.basePathSeriesOnAir + Constants.apiKey + "&language=pt-BR&page=1") else {
+            onError(.url)
+            return
+        }
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    print("No Error")
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else { return }
+                    do {
+                        let movies = try JSONDecoder().decode(SeriesOnAir.self, from: data)
+                        onComplete(movies)
                         print("FetchOK")
                     } catch let jsonErr {
                         onError(.invalidJSON)
