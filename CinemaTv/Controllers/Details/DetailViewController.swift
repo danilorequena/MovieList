4ddd1cc9da56c99e452507ba049e9bd571d57a9a
Danilo Requena <danilo.requena26@gmail.com>
2021-01-01 13:05:34 -0300

    fix load detail DiscoverSeries

--- CinemaTv/Controllers/Details/DetailViewController.swift
+++ CinemaTv/Controllers/Details/DetailViewController.swift
@@ -18,6 +18,7 @@ class DetailViewController: UIViewController, Storyboaded {
     var seriesPop: ResultSeries?
     var seriesOnAir: ResultSeriesOnAir?
     var discoverMovies: ResultDiscover?
+    var discoverSeries: ResultDiscoverSeries?
     var cardInfos: CardInfos?
     var viewModel: DetailViewModel?
     
@@ -37,6 +38,8 @@ class DetailViewController: UIViewController, Storyboaded {
             cardInfos?.setupCardPop(mainView: self.view, infos: seriesPop!)
         } else if seriesOnAir != nil {
             cardInfos?.setupCardOnAir(mainView: self.view, infos: seriesOnAir!)
+        } else if discoverSeries != nil {
+            cardInfos?.setupCardDiscoverSeries(mainView: self.view, infos: discoverSeries!)
         }
         setupImage()
     }
@@ -64,6 +67,16 @@ class DetailViewController: UIViewController, Storyboaded {
             }
         } else if seriesOnAir?.backdropPath != nil {
             if let backdropPath = seriesOnAir?.backdropPath {
+                guard let posterURL = URL(string: "https://image.tmdb.org/t/p/original/" + backdropPath) else {return}
+                do {
+                    let data = try Data(contentsOf: posterURL)
+                    self.imgBackdrop.image = UIImage(data: data)
+                } catch let jsonErr {
+                    print(jsonErr)
+                }
+            }
+        } else if discoverSeries?.backdropPath != nil {
+            if let backdropPath = discoverSeries?.backdropPath {
                 guard let posterURL = URL(string: "https://image.tmdb.org/t/p/original/" + backdropPath) else {return}
                 do {
                     let data = try Data(contentsOf: posterURL)
